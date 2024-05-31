e2bd93884172a4f1d2d45793a818e2900aeab5a6
Renan S Belchior <102825017+RenanBelchior@users.noreply.github.com>
2024-05-31 19:27:45 -0300

    Update scripts.js

--- Trabalho_Desenvolvimento_web/Front-end/scripts.js
+++ Trabalho_Desenvolvimento_web/Front-end/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    // Click do carrinho de compras
+    
     $("#cart-sidebar-toggler").click(function(){
         $("#cart-sidebar").toggleClass("open");
     });
 
-    // Fechar o carrinho de compras ao clicar fora dele
+    
     $(document).click(function(event) { 
         if(!$(event.target).closest('#cart-sidebar-toggler, #cart-sidebar').length) {
             if($('#cart-sidebar').hasClass('open')) {
@@ -13,7 +13,7 @@ $(document).ready(function(){
         }        
     });
 
-    // Adicionar Produto ao carrinho
+   
     $(".add-to-cart").click(function(e) {
         e.preventDefault();
         var id = $(this).data("id");
@@ -22,21 +22,21 @@ $(document).ready(function(){
         addToCart(id, name, price);
     });
 
-    // Função para adicionar produto ao carrinho
+    
     function addToCart(id, name, price) {
-        var itemHtml = '<li>'+name+' - R$ '+price+' <button class="btn btn-sm btn-danger remove-from-cart" data-id="'+id+'">Remover</button></li>';
+        var itemHtml = '<li data-id="'+id+'">'+name+' - R$ '+price+' <button class="btn btn-sm btn-danger remove-from-cart">Remover</button></li>';
         $("#cart-items").append(itemHtml);
         updateCartTotal();
     }
 
-    // Remover produto do carrinho
+    
     $(document).on("click", ".remove-from-cart", function() {
-        var id = $(this).data("id");
+        var id = $(this).closest("li").data("id");
         $(this).closest("li").remove();
         updateCartTotal();
     });
 
-    // Atualizar o total do carrinho
+    
     function updateCartTotal() {
         var total = 0;
         $("#cart-items li").each(function() {
@@ -45,4 +45,50 @@ $(document).ready(function(){
         });
         $("#cart-total").text("Total: R$ "+total.toFixed(2));
     }
+
+
+$('#checkoutForm').submit(function(event){
+    event.preventDefault(); 
+    var nome = $('#nome').val();
+    var contato = $('#contato').val();
+    var municipio = $('#municipio').val();
+    var bairro = $('#bairro').val();
+    var rua = $('#rua').val();
+    var numero = $('#numero').val();
+    var formaPagamento = $('#forma_pagamento').val();
+
+    
+    var itens = [];
+    $("#cart-items li").each(function() {
+        var id = $(this).data("id");
+        var name = $(this).text().split(" - R$ ")[0];
+        var price = $(this).text().split(" - R$ ")[1];
+        itens.push({id: id, nome: name, preco: price});
+    });
+
+    
+    $.ajax({
+        type: "POST",
+        url: "save_order.php",
+        data: { 
+            nome: nome,
+            contato: contato,
+            municipio: municipio,
+            bairro: bairro,
+            rua: rua,
+            numero: numero,
+            forma_pagamento: formaPagamento,
+            itens: itens
+        },
+        success: function(response) {
+            console.log("Pedido realizado com sucesso!");
+            console.log(response); 
+            window.location.href = 'confirmacao.html';
+        },
+        error: function(xhr, status, error) {
+            console.error("Erro ao enviar pedido: " + xhr.status + " - " + xhr.statusText);
+            console.error(xhr.responseText); 
+        }
+    });
+  });
 });
